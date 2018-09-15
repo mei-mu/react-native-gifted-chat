840e02d4c12563a6fb63329c3a5b43ccf451a283
suming <dpzxsm@sina.com>
2018-09-15 18:12:35 +0800

    聊天顺序

--- src/MessageContainer.js
+++ src/MessageContainer.js
@@ -171,6 +171,8 @@ export default class MessageContainer extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-start'
   },
   notInvertedContentContainerStyle: {
     justifyContent: 'flex-end',
