8c286f225f4a4a8d4a107774c782f996c938d225
suming <dpzxsm@sina.com>
2018-09-20 18:14:09 +0800

    统一安卓和ios的输入框高度

--- src/Constant.js
+++ src/Constant.js
@@ -2,7 +2,7 @@ import { Platform } from 'react-native';
 
 export const MIN_COMPOSER_HEIGHT = Platform.select({
   ios: 33,
-  android: 41,
+  android: 33,
 });
 export const MAX_COMPOSER_HEIGHT = 200;
 export const DEFAULT_PLACEHOLDER = 'Type a message...';
